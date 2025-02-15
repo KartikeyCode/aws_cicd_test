0e248d8d3c1e7dcec82134553a623d4ca82fe1e9
KartikeyCode <codekartikey@gmail.com>
2025-02-15 16:26:57 +0530

    add dockerfile

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "Server Running Successfully"
+    });
+});
+
+app.get('/test', (req, res) => {
+    res.send('Test Successful');
+});
+
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name} Nice to meet you `);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
